d30b73c1658f8f50dd55ca9bcafc0e7dade68f18
Gabriel Roca <blast.blackstone@gmail.com>
2024-03-21 13:52:27 +0000

    Update project_data.py
    Se optimizo el codigo por medio del uso de la libreria pathlib

--- app/utils/project_data.py
+++ app/utils/project_data.py
@@ -1,27 +1,17 @@
 import os
 
+from pathlib import Path
+
 def estructura_de_carpetas(directorio, prefijo=''):
-    """
-    Recursively generates the folder structure of a directory.
-
-    Args:
-        directorio (str): The path of the directory to generate the structure for.
-        prefijo (str, optional): The prefix to be added to each line of the structure. Defaults to ''.
-
-    Returns:
-        str: The generated folder structure.
-
-    """
-    resultado = ''  # Variable para acumular la estructura de las carpetas
-    with os.scandir(directorio) as entradas:
-        for entrada in entradas:
-            if entrada.is_dir() and not any(substring in entrada.name for substring in ["env", "venv", ".git", ".vscode", "__pycache__"]):
-                resultado += f"{prefijo}+ {entrada.name}\n"  # Añade el nombre del directorio al resultado
-                nuevo_prefijo = prefijo + "|  "
-                # Llamada recursiva y concatenación del resultado
-                resultado += estructura_de_carpetas(entrada.path, nuevo_prefijo)
-            elif entrada.is_file():
-                resultado += f"{prefijo}- {entrada.name}\n"  # Añade el nombre del archivo al resultado
+    directorio = Path(directorio)
+    resultado = ''
+    for entrada in directorio.iterdir():
+        if entrada.is_dir() and not entrada.name.startswith(('.', 'env', 'venv', '__pycache__')):
+            resultado += f"{prefijo}+ {entrada.name}\n"
+            nuevo_prefijo = prefijo + "|  "
+            resultado += estructura_de_carpetas(entrada, nuevo_prefijo)
+        elif entrada.is_file():
+            resultado += f"{prefijo}- {entrada.name}\n"
     return resultado
 
 
