175fd7d20e7f69a12b79649fe348375f24ef64d5
Gabriel Roca <blast.blackstone@gmail.com>
2024-03-31 22:58:11 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 
 # Lista de carpetas a ignorar
-FOLDERS_TO_IGNORE = ["__pycache__", "venv", "node_modules", "env"]
+FOLDERS_TO_IGNORE = ["__pycache__", "venv", "node_modules", "env","public", "build"]
