963e570d3374cce188becab84134d86d054f0a15
Gabriel Roca <blast.blackstone@gmail.com>
2024-09-13 13:37:41 +0000

    perf: Unnecessary condition is removed

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ import tkinter as tk
 
 from colorama import Fore
 
-from app.utils import genera_estructura_de_carpetas, extrae_contenido_archivos
-from app.utils import crea_prompt
+from app.utils import genera_estructura_de_carpetas, extrae_contenido_archivos, crea_prompt
 from app.ui import seleccionar_ruta, copiar_al_portapapeles, mostrar_arbol_directorios
 
 def main():
@@ -28,14 +27,16 @@ def main():
         print("No se seleccionó la carpeta. Terminando el programa.")
         return
         
+    # Genera y modifica el prompt    
     estructura = genera_estructura_de_carpetas(carpeta)    
-    archivos_seleccionados = mostrar_arbol_directorios(root, carpeta)  # Ahora captura los archivos seleccionados aquí
     
-    if archivos_seleccionados:
-        contenido_archivos = extrae_contenido_archivos(archivos_seleccionados)        
-        prompt_gpt = crea_prompt(path_prompt_base_txt, estructura, contenido_archivos)
-        print(Fore.RED + prompt_gpt + Fore.RESET)
-        copiar_al_portapapeles(root, prompt_gpt)
+    archivos_seleccionados = mostrar_arbol_directorios(root, carpeta)  # Ahora captura los archivos seleccionados aquí        
+    contenido_archivos = extrae_contenido_archivos(archivos_seleccionados)        
+    
+    prompt_gpt = crea_prompt(path_prompt_base_txt, estructura, contenido_archivos)
+    print(Fore.RED + prompt_gpt + Fore.RESET)
+    copiar_al_portapapeles(root, prompt_gpt)
+    
 
 if __name__ == "__main__":
     main()
