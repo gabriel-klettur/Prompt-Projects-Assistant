0656a38e5eddf3bc967ef86ed8bae333741c3c10
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-30 13:29:10 +0000

    Update config.py
    - Added 'build' and 'coverage' folders to 'FOLDERS_TO_IGNORE'

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 # config.py
 
 # Lista de carpetas a ignorar
-FOLDERS_TO_IGNORE = ["__pycache__", "venv", "node_modules", "env","public", "build"]
+FOLDERS_TO_IGNORE = ["__pycache__", "venv", "venvTesting", "node_modules", "env","public", "build", "coverage"]
